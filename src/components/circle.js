5691aea3dee4515caca88c6d60f5ba31f4cf9345
Rahul <rk255009@gmail.com>
2022-07-25 22:50:14 +0530

    export

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,13 @@
 // import using require
 
+var Shape= require("./shape");
+
 // declare class
 
+class Circle extends Shape{
+    constructor() { super(); this.calculateArea=()=> {} }
+};
 
 // export class using module.exports
+
+module.exports= Circle;
